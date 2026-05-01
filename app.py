6fa76c5d1b668462702d7cc566e23ae0f0e09f61
Narak <narakleng13579@gmail.com>
2026-05-01 23:19:31 +0700

    Casting Example of Python

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 #This is the first project testing of Python Programming Language.
-print('Hello Python!'),
-print("Hello Python!"),
 
-num1 = 10
+print("----------------Show User Information-----------------")
+name = "Narak Leng"
+age = 24
+gender = 'Male'
+nationaliy = "Cambodian"
 
-
-print(num1)
+print("User's Name :", name)
+print("User's Age :", age, "years old")
+print("User's Gender:", gender)
+print("User's Nationality:", nationaliy)
+print("His name is", name, "and his age is", age, "years old.")
---
+++ type-casting.py
@@ -0,0 +1,26 @@
+#Casting Example of Python Programming Language.
+
+num = int('100') #Casting age to integer
+print(num + 120) #Output will be 220
+#---------------------------------------------------------------
+age = 20
+text = "Age is " + str(age) #Casting age to string
+print(text) #Output will be "Age is 20"
+#---------------------------------------------------------------
+pi = 3.14159
+print(int(pi)) #Casting pi to integer, output will be 3
+#---------------------------------------------------------------
+number = 10
+print(float(number)) #Casting number to float, output will be 10.0
+#---------------------------------------------------------------    
+value = 5.5
+print(int(value)) #Casting value to integer, output will be 5
+#---------------------------------------------------------------    
+num_str = "123"
+print(int(num_str)) #Casting num_str to integer, output will be 123 
+#---------------------------------------------------------------
+num_float = 3.14
+print(str(num_float)) #Casting num_float to string, output will be "3.14"
+#---------------------------------------------------------------
+num_bool = 1
+print(bool(num_bool)) #Casting num_bool to boolean, output will be True
